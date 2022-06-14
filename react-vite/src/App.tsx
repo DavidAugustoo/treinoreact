0875fbf3d82190cbb00eb24b50c3011be7646e98
David Augusto <62584948+DavidAugustoo@users.noreply.github.com>
2022-06-14 17:26:47 -0300

    Configuração Tailwind

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -5,14 +5,8 @@ import * as C from './AppStyles';
 
 const App = () => {
 
-  return (
-      <C.Container bgColor="green">
-        <span><a className='link'>Texto</a> do componente</span>
-
-        <Button bg="red">Inscreva-se</Button>
-        <Button bg="grey" small>Cancelar</Button>
-        <C.Paragraph>Para se inscrever, clique no botão inscreva-se</C.Paragraph>
-      </C.Container>
+  return ( 
+    <div className="bg-black">...</div>
   );
 }
 
