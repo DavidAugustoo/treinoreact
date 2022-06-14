f5bbf08ddcbb60c29393de02eb1b9549ae76422a
David Augusto <62584948+DavidAugustoo@users.noreply.github.com>
2022-06-14 18:39:39 -0300

    Responsividade com Tailwind

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -6,7 +6,10 @@ import * as C from './AppStyles';
 const App = () => {
 
   return ( 
-    <div className="bg-black">...</div>
+    <div className="bg-indigo-700 text-white p-2 flex flex-col gap-4 items-center	">
+      <p className="text-center text-xl">Primeiros passos com Tailwind</p>
+      <button className="bg-cyan-500 hover:bg-cyan-600 px-2 py-1 rounded-lg text-sm w-full md:w-auto">Clique Aqui</button>
+    </div>
   );
 }
 
