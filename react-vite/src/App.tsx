308cb2fdcae366793c9bcd68d3a5c7e0d14a3755
David Augusto <62584948+DavidAugustoo@users.noreply.github.com>
2022-06-14 14:13:28 -0300

    Criando componentes com Styled Components

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,11 +1,16 @@
-import { useState } from 'react';
+// Primeiro forma de criar componentes
 import { Button } from './components/Button'
+// Segunda forma de criar compnentes
+import * as C from './AppStyles';
 
 const App = () => {
 
   return (
     <div>
-      <Button />
+      <C.Container>
+        <Button>Inscreva-se</Button>
+        <C.Paragraph>Para se inscrever, clique no botão inscreva-se</C.Paragraph>
+      </C.Container>
     </div>
   );
 }
---
+++ react-vite/src/AppStyles.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-color: gray;
+    width: 100%;
+`;
+
+export const Paragraph = styled.p`
+    color: red;
+`;
