d964e986e34cc9f7ecac4e6b8df574627ad4328a
David Augusto <62584948+DavidAugustoo@users.noreply.github.com>
2022-06-20 09:22:18 -0300

    Requisição Simples - Fetch

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,27 +1,36 @@
 import { ChangeEvent, useEffect, useState } from "react";
+import { Movie } from './types/Movie'
 
 const App = () => {
-  const [name, setName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [fullName, setFullName] = useState('');
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const loadMovies = () => {
+    fetch(`https://api.b7web.com.br/cinema/`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        setMovies(json);
+      });
+  }
 
   useEffect(() => {
-    setFullName(`${name} ${lastName}`);
-  }, [name, lastName]);
-      
-  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
-  }
-
-  const handleLastNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setLastName(e.target.value);
-  }
-
+    loadMovies();
+  },[])
+    
   return ( 
     <div>
-      <input type="text" value={name} placeholder="Digite seu nome" onChange={handleNameChange}/>
-      <input type="text" value={lastName} placeholder="Digite seu sobrenome" onChange={handleLastNameChange}/>
-      <p>Nome completo: {fullName}</p>
+      <button className="block bg-blue-400 p-2 rounded" onClick={loadMovies}>Carregar Filmes</button>
+
+      Total de filmes: {movies.length}
+      <div className="grid grid-cols-6 gap-3">
+        {movies.map((item, key) => {
+          return <div key={key}>
+            <img src={item.avatar} alt="" className="w-32 block"/>
+            {item.titulo}
+          </div>
+        })}
+      </div>
     </div>
   );
 }
