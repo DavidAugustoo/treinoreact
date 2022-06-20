31ea254e7c2291794b15515686922db452a4de7a
David Augusto <62584948+DavidAugustoo@users.noreply.github.com>
2022-06-20 10:15:40 -0300

    Loading

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -3,10 +3,13 @@ import { Movie } from './types/Movie'
 
 const App = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const[loading, setLoading] = useState(false);
 
   const loadMovies = async () => {
+    setLoading(true);
     let response = await fetch(`https://api.b7web.com.br/cinema/`);
     let json = await response.json();
+    setLoading(false);
     setMovies(json);
   }
 
@@ -18,8 +21,17 @@ const App = () => {
     <div>
       <button className="block bg-blue-400 p-2 rounded" onClick={loadMovies}>Carregar Filmes</button>
 
-      Total de filmes: {movies.length}
-      <div className="grid grid-cols-6 gap-3">
+     {!loading && 
+      <div>
+        Total de filmes: {movies.length}
+      </div>
+     }
+    
+      {loading && 
+        <div>Carregando...</div>
+      }
+
+      <div className="grid grid-cols-6 gap-3">        
         {movies.map((item, key) => {
           return <div key={key}>
             <img src={item.avatar} alt="" className="w-32 block"/>
