72b07acc6f396e6cb4503032de04e639f94c1c8b
David Augusto <62584948+DavidAugustoo@users.noreply.github.com>
2022-06-21 18:02:13 -0300

    Requisições - AXIOS

--- react-vite/src/api.ts
+++ react-vite/src/api.ts
@@ -1,18 +1,18 @@
-const BASE = 'https://jsonplaceholder.typicode.com/posts'
+import axios from 'axios';
+
+const http = axios.create({
+    baseURL: "https://jsonplaceholder.typicode.com/posts"
+});
 
 export const api = {
     getAllPosts: async () => {
-        let response = await fetch(`${BASE}/posts`);
-        let json = await response.json();
-        return json;
+        let response = await http.get('/posts');
+        return response.data;
     },
     addNewPost: async (title: string, body: string, userId: number) => {
-        let response = await fetch(`${BASE}/posts`, {
-        method: 'POST',
-        body: JSON.stringify({ body, title, userId }),
-        headers: { 'Content-Type': 'aplication/json' }
-      });
-      let json = await response.json();
-      return json;
+        let response = await http.post('/posts', {
+            title, body, userId
+        });
+        return response.data;
     }
 }
