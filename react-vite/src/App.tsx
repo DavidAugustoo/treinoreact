8ef8415bb93f792c5ec5a95d8e5deb5b93609bbc
David Augusto <62584948+DavidAugustoo@users.noreply.github.com>
2022-06-21 22:04:56 -0300

    Situação 1 - useReducer

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,33 +1,9 @@
 import { useReducer } from "react";
-
-type reducerState = {
-  count: number;
-}
-
-type reducerAction = {
-  type: string;
-}
-const initialState = { count: 0 };
-const reducer = (state: reducerState, action: reducerAction ) => {
-  switch (action.type) {
-    case 'ADD':
-      return {...state, count: state.count + 1}
-    break; 
-    case 'DEL':
-      if(state.count > 0) {
-        return {...state, count: state.count - 1}
-      }
-    break;
-    case 'RESET':
-      return initialState
-    break;
-  }
-  return state;
-}
+import { UseContagem } from "./reducers/Contagem";
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
+  const [state, dispatch] = UseContagem();
+  
   return (
     <div>
       Contagem: {state.count}
