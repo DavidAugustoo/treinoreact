acaead2c51d82c48f0b7f2097d67b9138c3dff4a
David Augusto <davidribeiro086@gmail.com>
2022-07-09 14:01:21 -0300

    Conceito de css.modules

--- Projeto 1/01-fundamentos-reactjs/src/App.jsx
+++ Projeto 1/01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,24 @@
 import { Post } from './Post';
+import { Header } from './components/Header';
+
+import './styles.css'
 
 function App() {
   return (
-    <Post />
+    <div>
+      <Header />
+
+      <Post 
+        author="David Augusto" 
+        content=" Odit, ducimus orem, ipsum dolor sit amet Lconsectetur adipisicing elit.?"
+      />
+
+      <Post 
+        author="Lucas Amadeu" 
+        content="Lconsectetur adipisicing elit. Odit, ducimus orem, ipsum dolor sit amet ?"
+      />
+    </div>
+    
   )
 }
 
