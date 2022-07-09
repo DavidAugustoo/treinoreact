d1003f80694f185095f838ee17632112b6404910
David Augusto <davidribeiro086@gmail.com>
2022-07-09 19:22:56 -0300

    Componente: Sidebar

--- Projeto 1/01-fundamentos-reactjs/src/App.jsx
+++ Projeto 1/01-fundamentos-reactjs/src/App.jsx
@@ -1,24 +1,33 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 
-import './global.css'
+import styles from './App.module.css';
+
+import './global.css';
+
+
 
 function App() {
   return (
     <div>
       <Header />
 
-      <Post 
-        author="David Augusto" 
-        content=" Odit, ducimus orem, ipsum dolor sit amet Lconsectetur adipisicing elit.?"
-      />
+      <div className={styles.wrapper}>
+       <Sidebar />
+        <main>
+          <Post 
+            author="David Augusto" 
+            content=" Odit, ducimus orem, ipsum dolor sit amet Lconsectetur adipisicing elit.?"
+          />
 
-      <Post 
-        author="Lucas Amadeu" 
-        content="Lconsectetur adipisicing elit. Odit, ducimus orem, ipsum dolor sit amet ?"
-      />
+          <Post 
+            author="Lucas Amadeu" 
+            content="Lconsectetur adipisicing elit. Odit, ducimus orem, ipsum dolor sit amet ?"
+          />
+        </main>
+      </div>
     </div>
-    
   )
 }
 
---
+++ Projeto 1/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -0,0 +1,31 @@
+import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react';
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img 
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+            />
+
+            <div className={styles.profile}>
+
+                <img
+                    className={styles.avatar} 
+                    src="https://media-exp1.licdn.com/dms/image/C4E03AQGl-4pJk9Zpbw/profile-displayphoto-shrink_800_800/0/1642889729830?e=1662595200&v=beta&t=5bOEzleINWi8VNuOvD8CWBT6V4SZktwUvKQHMOrN1xs" 
+                />
+
+                <strong> David Augusto</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20}/> 
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
