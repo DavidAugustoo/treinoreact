ff2c30eb0b5b3cd6221085c615b70b5efdab9665
David Augusto <davidribeiro086@gmail.com>
2022-07-09 20:19:09 -0300

    Formulário de comentários

--- Projeto 1/01-fundamentos-reactjs/src/components/Post.jsx
+++ Projeto 1/01-fundamentos-reactjs/src/components/Post.jsx
@@ -29,6 +29,19 @@ export function Post() {
                     <a>#rocketseat</a>{' '}
                 </p>
             </div>
+
+
+            <form className={styles.commentForm}> 
+                <strong>Deixe seu feedback</strong>
+                <textarea 
+                    placeholder="Deixe seu comentário"
+                />
+                
+                <footer>
+                    <button type="submit">Comentar</button> 
+                </footer>
+                
+            </form>
         </article>
     )
 }
