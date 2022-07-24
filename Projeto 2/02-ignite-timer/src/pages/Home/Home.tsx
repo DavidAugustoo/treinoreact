05bfdb885b7cb998b26fdc7d1aeea64452776675
David Augusto <davidribeiro086@gmail.com>
2022-07-24 20:42:12 -0300

    Cadastrando novo ciclo - Projeto Timer

--- Projeto 2/02-ignite-timer/src/pages/Home/Home.tsx
+++ Projeto 2/02-ignite-timer/src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { v4 as uuidv4 } from 'uuid'
 import * as zod from 'zod'
 
 import {
@@ -12,6 +13,7 @@ import {
   TaskInput,
   MinutesAmountInput,
 } from './styles'
+import { useState } from 'react'
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
@@ -21,9 +23,18 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, 'O Ciclo precisa ser de no máximo 60 minutos.'),
 })
 
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+}
+
 type NewCyCleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<String | null>(null)
+
   const { register, handleSubmit, watch, reset } = useForm<NewCyCleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
@@ -32,11 +43,24 @@ export function Home() {
     },
   })
 
-  function handleCreateNewCycle(data: any) {
-    console.log(data)
+  function handleCreateNewCycle(data: NewCyCleFormData) {
+    const id = uuidv4()
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    }
+
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
 
     reset()
   }
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  console.log(activeCycle)
 
   const task = watch('task')
   const isSubmitDisabled = !task
