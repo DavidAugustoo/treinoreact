324223459fdcec0c8798bf9b22cbc3fcaf2f4081
David Augusto <davidribeiro086@gmail.com>
2022-07-31 19:02:34 -0300

    Utilizando contextApi - Projeto Timer

--- Projeto 2/02-ignite-timer/src/pages/History/styles.ts
+++ Projeto 2/02-ignite-timer/src/pages/History/styles.ts
@@ -22,44 +22,45 @@ export const HistoryList = styled.div`
     width: 100%;
     border-collapse: collapse;
     min-width: 600px;
-  }
 
-  th {
-    background-color: ${(props) => props.theme['gray-600']};
-    padding: 1rem;
-    text-align: left;
-    color: ${(props) => props.theme['gray-100']};
-    font-size: 0.875rem;
-    line-height: 1.6rem;
+    th {
+      background-color: ${(props) => props.theme['gray-600']};
+      padding: 1rem;
+      text-align: left;
+      color: ${(props) => props.theme['gray-100']};
+      font-size: 0.875rem;
+      line-height: 1.6;
 
-    &:first-child {
-      border-top-left-radius: 8px;
-      padding-left: 1.5rem;
+      &:first-child {
+        border-top-left-radius: 8px;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        border-top-right-radius: 8px;
+        padding-right: 1.5rem;
+      }
     }
 
-    &:last-child {
-      border-top-right-radius: 8px;
-      padding-right: 1.5rem;
-    }
-  }
+    td {
+      background-color: ${(props) => props.theme['gray-700']};
+      border-top: 4px solid ${(props) => props.theme['gray-800']};
+      padding: 1rem;
+      font-size: 0.875rem;
+      line-height: 1.6;
 
-  td {
-    background-color: ${(props) => props.theme['gray-700']};
-    border-top: 4px solid ${(props) => props.theme['gray-800']};
-    padding: 1rem;
-    font-size: 0.875rem;
-    line-height: 1.6rem;
+      &:first-child {
+        width: 50%;
+        padding-left: 1.5rem;
+      }
 
-    &:first-child {
-      width: 50%;
-      padding-left: 1.5rem;
-    }
-
-    &:last-child {
-      padding-right: 1.5rem;
+      &:last-child {
+        padding-right: 1.5rem;
+      }
     }
   }
 `
+
 const STATUS_COLORS = {
   yellow: 'yellow-500',
   green: 'green-500',
@@ -79,7 +80,7 @@ export const Status = styled.span<StatusProps>`
     content: '';
     width: 0.5rem;
     height: 0.5rem;
-    border-radius: 50%;
+    border-radius: 9999px;
     background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
   }
 `
